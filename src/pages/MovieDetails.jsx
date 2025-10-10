a75b7f86d584dadd0a50cbc9fbaab683542ebe7a
Abdul Kadir <abdulkadir80949@gmail.com>
2025-10-10 08:48:53 +0530

    Show more fixed

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -8,7 +8,6 @@ import DateSelect from "../components/DateSelect";
 import MovieCard from "../components/MovieCard";
 
 const MovieDetails = () => {
-
   const navigate = useNavigate();
 
   const { id } = useParams();
@@ -73,47 +72,45 @@ const MovieDetails = () => {
             <Heart className={`w-5 h-5`} />
           </button>
         </div>
+      </div>
+
+      {/* Showing Cast */}
+      <p className="text-lg font-medium mt-20">Your Favorite Cast</p>
+
+      <div className="overflow-x-auto no-scrollbar mt-8 pb-4">
+        <div className="flex items-center gap-4 w-max px-4">
+          {show.movie.casts.slice(0, 12).map((cast, index) => (
+            <div key={index} className="flex flex-col items-center text-center">
+              <img
+                src={cast.profile_path}
+                alt={cast.name}
+                className="rounded-full h-20 md:h-20 aspect-square object-cover"
+              />
+              <p className="font-medium text-xs mt-3">{cast.name}</p>
+            </div>
+          ))}
         </div>
-
-
-        {/* Showing Cast */}
-        <p className="text-lg font-medium mt-20">Your Favorite Cast</p>
-
-        <div className="overflow-x-auto no-scrollbar mt-8 pb-4">
-          <div className="flex items-center gap-4 w-max px-4">
-            {show.movie.casts.slice(0, 12).map((cast, index) => (
-              <div
-                key={index}
-                className="flex flex-col items-center text-center"
-              >
-                <img
-                  src={cast.profile_path}
-                  alt={cast.name}
-                  className="rounded-full h-20 md:h-20 aspect-square object-cover"
-                />
-                <p className="font-medium text-xs mt-3">{cast.name}</p>
-              </div>
-            ))}
-          </div>
-        </div>
+      </div>
 
       <DateSelect dateTime={show.dateTime} id={id} />
 
-      {/* Code for recommended movies for user (you may also like) */}
+      {/* recommended movies for user (you may also like) */}
+
       <p className="text-lg font-medium mt-20 mb-8">You May Also Like</p>
       <div className="flex flex-wrap max-sm:justify-center gap-8">
-            {dummyShowsData.slice(0,4).map((movie,index)=>(
-              <MovieCard key={index} movie={movie} />
-            ))}
-      </div>
-      <div className="flex justify-center mt-20">
-        <button onClick={()=>navigate('/movies')} className="px-10 py-3 text-sm bg-primary hover:bg-primary-dull transition rounded-md font-medium cursor-pointer">
+        {dummyShowsData.slice(0, 4).map((movie, index) => (
+          <MovieCard key={index} movie={movie} />
+        ))}
+
+        <div className="flex flex-wrap justify-center mt-20"> 
+          <button
+            onClick={() => navigate("/movies")}
+            className="px-10 py-3 text-sm bg-primary hover:bg-primary-dull transition rounded-md font-medium cursor-pointer"
+          >
             Show more
-        </button>
-
-
+          </button>
+        </div>
       </div>
-
     </div>
   ) : (
     <div>Loading...</div>
