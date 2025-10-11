7449f04f814a8d925da8760da5db78818a9c138f
Abdul <abdulkadir80949@gmail.com>
2025-10-12 00:46:45 +0530

    Admin Booking page has been completed

--- src/pages/admin/ListBookings.jsx
+++ src/pages/admin/ListBookings.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { dummyBookingData } from '../../assets/assets';
+import Loading from '../../components/Loading';
+import Title from '../../components/admin/Title';
 
 const ListBookings = () => {
-  return (
-    <div>ListBookings</div>
-  )
+  const currency = import.meta.env.VITE_CURRENCY
+  
+    const [bookings, setBookings] = useState([])
+    const [isloading, setIsLoading] = useState(true);
+
+    const getAllBookings = async () =>{
+      setBookings(dummyBookingData)
+      setIsLoading(false)
+    }
+
+    useEffect(()=>{
+      getAllBookings();
+    },[])
+  return ! isloading ?  (
+   <>
+   <Title text1='List' text2="Bookings"/>
+   <div className='max-w-4xl mt-6 overflow-x-auto'>
+
+   </div>
+   </>
+  ):<Loading/>;
 }
 
 export default ListBookings
